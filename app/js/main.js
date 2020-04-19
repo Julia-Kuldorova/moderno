764733c3920fa32418190c44770dd11a59850c0c
Julia-Kuldorova <julia.julivne@gmail.com>
2020-04-19 16:02:58 +0300

    added new-items

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,8 @@ $(function(){
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
-        readOnly: true
+        readOnly: true,
+        ratedFill: "#ffa726"
       });
     
     $(".product-slider__inner").slick({
@@ -13,6 +14,27 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
         infinite: true
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+    
+    $(".icon-th-list").on("click", function(){
+        $(".product-page__content-item").addClass('list');
+        $(".icon-th-list").addClass('active');
+        $(".icon-th-large").removeClass('active');
+    });
+
+    $(".icon-th-large").on("click", function(){
+        $(".product-page__content-item").removeClass('list');
+        $(".icon-th-large").addClass('active');
+        $(".icon-th-list").removeClass('active');
     });
 
 
